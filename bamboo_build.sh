de07256f52f4c22fcee7b54371514aff1647eced
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2017-03-02 00:00:31 -0800

    DEP-187/now stealing from anaconda

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash -ex
 
 source /mnt/software/Modules/current/init/bash
-module load openblas
-module load hdf5-tools/1.8.11
-
 mkdir -p tmp
 /opt/python-2.7.9/bin/python /mnt/software/v/virtualenv/13.0.1/virtualenv.py tmp/venv
+PIP_CACHE=$PWD/.pip_cache
+find $PIP_CACHE -name '*-linux_x86_64.whl' -delete || true
+
 source tmp/venv/bin/activate
 
-(cd repos/pbcommand && make install)
+rsync -avx /mnt/software/a/anaconda2/4.2.0/pkgs/mkl-11.3.3-0/lib/             tmp/venv/lib/
+rsync -avx /mnt/software/a/anaconda2/4.2.0/pkgs/numpy-1.11.1-py27_0/bin/      tmp/venv/bin/
+rsync -avx /mnt/software/a/anaconda2/4.2.0/pkgs/numpy-1.11.1-py27_0/lib/      tmp/venv/lib/
+rsync -avx /mnt/software/a/anaconda2/4.2.0/pkgs/scipy-0.18.1-np111py27_0/lib/ tmp/venv/lib/
+
+module load hdf5-tools/1.8.11
 export HDF5_DIR=/mnt/software/h/hdf5-tools/1.8.11
-export PIP_CACHE=$PWD/.pip_cache
-find $PIP_CACHE -name '*-linux_x86_64.whl' -delete || true
-pip install -r requirements-ci.txt
-pip install -r requirements-dev.txt
+(cd repos/pbcommand && make install)
+pip --cache-dir=$PIP_CACHE install -r requirements-ci.txt
+pip --cache-dir=$PIP_CACHE install -r requirements-dev.txt
 (cd repos/pbcore && make install)
 
 pip install --no-index --install-option="--install-scripts=$PWD/tmp/venv/bin" ./
