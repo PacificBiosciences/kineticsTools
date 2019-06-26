74b63efc1841a0e6b5ef75d9223daaf198df3ef0
natechols <nathaniel.echols@gmail.com>
2019-06-26 13:07:42 -0700

    restore old bigwig gather task from pbcoretools, minus TC support

---
+++ test/test_gather_bigwig.py
@@ -0,0 +1,85 @@
+
+import subprocess
+import unittest
+import tempfile
+import logging
+import os.path as op
+import sys
+
+try:
+    import pyBigWig
+except ImportError:
+    pyBigWig = None
+
+from pbcommand.pb_io.common import load_pipeline_chunks_from_json, \
+    write_pipeline_chunks
+from pbcommand.pb_io.report import load_report_from_json
+from pbcommand.models import PipelineChunk
+
+from kineticsTools.tasks.gather_bigwig import gather_bigwig
+
+log = logging.getLogger(__name__)
+
+def _generate_chunk_output_file(i=None):
+    if pyBigWig is None:
+        raise unittest.SkipTest("pyBigWig not installed")
+    records = [
+        ("chr1", 4, 5, 0.45),
+        ("chr2", 8, 9, 1.0),
+        ("chr2", 9, 10, 6.7),
+        ("chr1", 1, 2, 1.5),
+        ("chr1", 2, 3, 4.5),
+        ("chr1", 3, 4, 1.9)
+    ]
+    fn = tempfile.NamedTemporaryFile(suffix=".bw").name
+    _records = records[(i*3):(i*3)+3]
+    assert len(_records) == 3
+    ranges = {}
+    for rec in _records:
+        seqid = rec[0]
+        pos = rec[1]
+        ranges.setdefault(seqid, (sys.maxsize, 0))
+        ranges[seqid] = (min(ranges[seqid][0], pos),
+                         max(ranges[seqid][1], pos))
+    bw = pyBigWig.open(fn, "w")
+    regions = [(s, ranges[s][1]+1) for s in sorted(ranges.keys())]
+    bw.addHeader(regions)
+    bw.addEntries([rec[0] for rec in _records],
+                  [rec[1]-1 for rec in _records],
+                  ends=[rec[2]-1 for rec in _records],
+                  values=[rec[3] for rec in _records])
+    bw.close()
+    return fn
+
+
+@unittest.skipUnless(pyBigWig is not None, "pyBigWig not installed")
+class TestGatherBigwig(unittest.TestCase):
+    NCHUNKS = 2
+
+    @classmethod
+    def setUpClass(cls):
+        cls._data_files = [_generate_chunk_output_file(i=i)
+                           for i in range(cls.NCHUNKS)]
+
+    def validate_output(self, output_file):
+        bw = pyBigWig.open(output_file)
+        nrec = bw.header()["nBasesCovered"]
+        self.assertEqual(nrec, 6, "{n} != 6".format(n=nrec))
+        self.assertAlmostEqual(bw.stats("chr1", 2, 3)[0], 1.9, places=5)
+        self.assertAlmostEqual(bw.stats("chr2", 7, 8)[0], 1.0, places=5)
+
+    def test_gather_bigwig(self):
+        ofn = tempfile.NamedTemporaryFile(suffix=".bw").name
+        rc = gather_bigwig(self._data_files, ofn)
+        self.validate_output(ofn)
+
+    def test_gather_bigwig_cli(self):
+        tmp_dir = tempfile.mkdtemp()
+        ofn = op.join(tmp_dir, "gathered.bw")
+        args = ["python", "-m", "kineticsTools.tasks.gather_bigwig",
+                ofn] + self._data_files
+        log.info("Output will be in %s", tmp_dir)
+        with open(op.join(tmp_dir, "stdout"), "w") as stdout:
+            with open(op.join(tmp_dir, "stderr"), "w") as stderr:
+                subprocess.check_call(args, stdout=stdout, stderr=stderr)
+        self.validate_output(ofn)
